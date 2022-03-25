1dc7cf523b84c08e8f26bd668c5dba9baa2d6934
Sreepathink <77881254+sreepathink@users.noreply.github.com>
2022-03-26 01:10:46 +0530

    Update Comparing_Numbers.sh

--- Comparing_Numbers.sh
+++ Comparing_Numbers.sh
@@ -1,14 +1,16 @@
-read X;
-read Y;
-# Here in condition will be placed under two square bracks and round bracks
-# In condition we can use both -gt / > symbol, Its upto our discretion.
-if [[($X -gt $Y)]];  
-then 
-echo  X is greater than Y ;
-elif [[($X -lt $Y)]];
-then 
-echo  X is less than Y ;
-elif [[($X -eq $Y)]];
-then 
-echo  X is equal to Y ;
+read x; read y; read z;
+
+xy= $x + $y;
+yz= $y + $z;
+zx = $z + $x;
+
+if [[(1 -le $x) && ( $x -le 1000) && (1 -le $y) && ($y -le 1000) && (1 -le $z) && ($z -le 1000) && ($xy -ge $z) && ($yz -ge $x) && ($zx -ge $y)]]; then echo
+
+elif [[($x -eq $y) && ($y -eq $z)]]; then echo "EQUILATERAL"
+# Whitespace between the Variable and == is mandatory if not it will execute in correctly
+
+elif [[($x == $y) || ($y == $z) || ( $z == $x )]]; then echo "ISOSCELES" 
+
+else echo "SCALENE"
+
 fi
