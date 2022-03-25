96b07103e59f630bea5e40d795a29df32568315c
Sreepathink <77881254+sreepathink@users.noreply.github.com>
2022-03-25 15:07:00 +0530

    Create Getting_started_with_conditionals.sh

---
+++ Getting_started_with_conditionals.sh
@@ -0,0 +1,10 @@
+read var;
+#read the Variable,note: in shell only string is the data type
+#Below we given multiple conditions.
+if [[( Y == $var)||(y == $var)]];
+then
+echo "YES";
+elif [[(N == $var)||(n == $var)]];
+then
+ echo "NO";
+ fi
